954bc8b936155f6c05a6c4e43790731db9ea8685
andreagonz <andreagonz@ciencias.unam.mx>
2016-10-04 01:17:29 -0500

    Mutacion terminada.. Hasta donde ve el ojo desprevenido

--- src/operador/mutacion/AVMutation.java
+++ src/operador/mutacion/AVMutation.java
@@ -1,8 +1,7 @@
 import gaframework.*;
 import java.util.Random;
-import java.util.BitSet;
 
-public class AVMutation<G> implements MutationOp<G> {
+public class AVMutation implements MutationOp<Integer> {
 
     private double prob;
     private Random r;
@@ -12,19 +11,42 @@ public class AVMutation<G> implements MutationOp<G> {
 	this.r = new Random(seed);
     }
 
-    public Genotype<G> mutate(Genotype<G> g) {
+    public Genotype<Integer> mutate(Genotype<Integer> g) {
 	int gsize = g.size();
-	Genotype<G> out = new Genotype<>(gsize);
-	Bitset mascara = new BitSet(gsize);
+        Integer[] tmp = new Integer[gsize];
+        int j = 0;
+        
 	for (int i = 1; i < gsize; i++)
-	    if (r.nextDouble() < prob)
-		mascara.set(i);	    
-	if(mascara.cardinality() == 1){
-	    int i = r.nextInt(mascara.size() - 1) + 1;
-	    while(mascara.get(i) != True)
-		i = r.nextInt(mascara.size() - 1) + 1;
+	    if (r.nextDouble() < prob) {
+                tmp[j] = g.getGene(i);
+                g.setGene(i,null);
+                j++;
+            }
+        
+        if(j == 1){
+	    int i = r.nextInt(gsize - 1) + 1;
+	    while(g.getGene(i) == null)
+		i = r.nextInt(gsize - 1) + 1;
+            tmp[j] = g.getGene(i);
+            g.setGene(i,null);
 	}
+
+        j = 0;
+        while(j < gsize - 1 && tmp[j + 1] != null) {
+            Integer in = tmp[j];
+            tmp[j] = tmp[j + 1];
+            tmp[j + 1] = in;
+            j++;
+        }
+
+        j = 0;
+        for(int i = 1; i < gsize; i++) {
+            if(g.getGene(i) == null) {
+                g.setGene(i, tmp[j]);
+                j++;
+            }
+        }
 	
-	return out;
+	return g;
     }
 }
