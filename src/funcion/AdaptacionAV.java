e7fe3b00a5320dec9dbc597aed917da14094e034
andreagonz <andreagonz@ciencias.unam.mx>
2016-10-04 19:59:25 -0500

    Cambio a Funcion objetivo para que no haga los sorts horribles

--- src/funcion/AdaptacionAV.java
+++ src/funcion/AdaptacionAV.java
@@ -9,9 +9,8 @@ public class AdaptacionAV implements FitnessFunction<Integer>{
 
     public double evaluate(Phenotype<Integer> p){
         double fitness = 0;
-        for(int i = 0; i < p.size();i++)
-            if(i < p.size()-1)
-                fitness += dt.getDistanceBetween(p.getAllele(i),p.getAllele(i+1));
+        for(int i = 0; i < p.size() - 1;i++)
+            fitness += dt.getDistanceBetween(p.getAllele(i),p.getAllele(i+1));
         fitness += dt.getDistanceBetween(p.getAllele(0),p.getAllele(p.size()-1));
         return fitness;
     }
--- src/funcion/ObjetivoAV.java
+++ src/funcion/ObjetivoAV.java
@@ -1,14 +1,20 @@
 import gaframework.*;
 public class ObjetivoAV implements ObjectiveFunction<Integer,Integer>{
     public void evaluate(Population<Integer,Integer> p){
-        double maxfit,minfit = 0;
-        maxfit = p.getBestIndividual().getFitness();
-        minfit = p.getWorstIndividual().getFitness();
+
+        double maxfit = 0, minfit = 0;
+        for(int i = 0; i < p.size(); i++) {
+            if(minfit > p.getIndividual(i).getFitness())
+                minfit = p.getIndividual(i).getFitness();
+            if(maxfit < p.getIndividual(i).getFitness())
+                maxfit = p.getIndividual(i).getFitness();                
+        }
+            
         for(int i = 0; i < p.size(); i++){
             Individual<Integer, Integer> ind;
             double eval = 0;
             ind = p.getIndividual(i);
-            eval = (maxfit+minfit)-ind.getFitness();
+            eval = (maxfit + minfit) - ind.getFitness();
             ind.setObjectiveEvaluation(eval);
         }
     }
